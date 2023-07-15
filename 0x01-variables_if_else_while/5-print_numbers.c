8b5f6c1d7fdd857dc54bcc803ec6ad735155cdfc
Tasneem Tarek <tashamed27@gmail.com>
2023-07-15 00:24:07 -0700

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ *main - Entry point 
+ *
+ *Return: Always o (success)
+ */  
+int main(void)
+{
+int digit = 0;
+while (digit <= 9)
+{
+printf("%i", digit);
+digit++;
+}
+printf("\n");
+}
