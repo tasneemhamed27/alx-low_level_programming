cda0c569ba22f923127238681578c3ad8b1eec23
Tasneem Tarek <tashamed27@gmail.com>
2023-07-14 22:08:47 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ *main - Entry point 
+ *
+ *Return: Always o (success)
+ */  
+int main(void)
+{
+char ch = 'a';
+while (ch = 'z')
+{
+putchar(ch);
+ch++;
+}
+putchar('\n');
+return (0);
+}
