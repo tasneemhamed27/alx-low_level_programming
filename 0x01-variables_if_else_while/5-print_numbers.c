3d794ec6198037b733e9c4ba908f97344905be5b
Tasneem Tarek <tashamed27@gmail.com>
2023-07-15 00:25:57 -0700

    5-print_numbers.c

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,11 +1,11 @@
 #include <stdlib.h>
 #include <time.h>
-#include <stdio.h>
+#include<stdio.h>
 /**
  *main - Entry point 
  *
  *Return: Always o (success)
- */  
+*/  
 int main(void)
 {
 int digit = 0;
