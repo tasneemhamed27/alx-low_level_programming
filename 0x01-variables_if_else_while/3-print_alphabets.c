30d33d4e1815abfade848a09b793aef510875d25
Tasneem Tarek <tashamed27@gmail.com>
2023-07-14 22:24:44 -0700

    3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ *main - Entry point 
+ *
+ *Return: Always o (success)
+ */  
+int main(void)
+{
+char ch = 'a';
+char CH = 'A';
+while (ch = 'z')
+{
+putchar(ch);
+ch++;
+}
+while (ch = 'z')
+{
+putchar(CH);
+ch++;
+}
+putchar('\n');
+return (0);
+}
