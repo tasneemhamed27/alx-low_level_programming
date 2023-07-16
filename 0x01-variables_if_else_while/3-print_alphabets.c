ef19caf66943389a1902cffd24dcba4fa363bac8
Tasneem Tarek <tashamed27@gmail.com>
2023-07-15 23:00:19 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ *main - Entry point
+ *
+ *Return: Always 0 (success)
+ */
+int main(void)
+{
+char ch = 'a';
+char CH = 'A';
+while (ch <= 'z')
+{
+putchar(ch);
+ch++;
+}
+while (CH <= 'Z')
+{
+putchar(CH);
+CH++;
+}
+putchar('\n');
+return (0);
+}
+
