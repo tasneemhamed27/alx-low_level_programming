473030f153e706b7ee1f126f4860cb3f2d559ba6
Tasneem Tarek <tashamed27@gmail.com>
2023-07-19 04:48:04 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * print_alphabet - function to print alphabet in lowercase
+ *
+ */
+void print_alphabet(void)
+{
+char ch;
+for (ch = 'a'; ch <= 'z'; ch++)
+_putchar(ch);
+_putchar('\n');
+}
