738c5d94e3b9ee2e91f18ed96dd5301d7a2bf0be
Tasneem Tarek <tashamed27@gmail.com>
2023-07-27 02:51:14 -0700

    2-strncpy

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -7,14 +7,13 @@
  */
 char *_strncpy(char *dest, char *src, int n)
 {
-int index, src_len = 0;
-while (src[index++])
-src_len++;
-for (index = 0; src[index] && index < n; index++)
-dest[index] = src[index];
-for (index = src_len; index < n; index++)
-dest[index] = '\n';
-
+int i;
+for (i = 0; i < n && src[i] != '\0'; i++)
+dest[i] = src[i];
+while (i < n)
+{
+dest[i] = '\0';
+i++;
+}
 return (dest);
 }
-
